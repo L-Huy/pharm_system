f6136320ddf8072988eabce752bc2601e69d01d4
jennie phem <jennie.phem@gmail.com>
2023-03-28 19:43:52 +0700

    test git ui

--- src/main/java/com/example/JavaAssessment3Application.java
+++ src/main/java/com/example/JavaAssessment3Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaAssessment3Application {
 
     public static void main(String[] args) {
+        // test commit
         SpringApplication.run(JavaAssessment3Application.class, args);
     }
 
